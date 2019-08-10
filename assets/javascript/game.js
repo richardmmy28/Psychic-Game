026dd50a156353dca5d29a04c5505c5ec9e39b3f
Richard Mazel <richardmmy28@gmail.com>
2019-08-10 12:54:11 -0700

    Finished homework using reset function to make it replayable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,37 +4,51 @@ var wins = 0;
 var losses = 0;
 var guesses = 9;
 var lettersGuessed = [];
+var computerChoice = reset();
 
-
-var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+function reset(){
+   return letters[Math.floor(Math.random() * letters.length)]
+}
 
 console.log(computerChoice);
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
+    //This outputs letters used//
     lettersGuessed.push(userGuess);
     //This if statement allows user to choose letter.//
     if(userGuess === computerChoice){
         wins++;
         alert("you guessed the right letter");
+        start(); 
     
     //This elseIf statement keeps track of how many guesses and passes an alert if you're guess is wrong //
-    }else if(guesses > 1){
+    }else if(userGuess != computerChoice){
         guesses--;
         alert("Guess Again");
     }
-    else {}
+    
         
     //this if statment equates to game loss//
-    if(guesses < 1){
+    if(guesses === 0){
         losses++;
+        start();
     }
+function start(){
+    guesses = 9;
+    computerChoice = reset();
+    lettersGuessed = [];
+    
+
+
+}
 
    
 document.getElementById("wins").innerHTML = "Wins: " + wins;
 document.getElementById("losses").innerHTML = "losses: " + losses;
 document.getElementById("guesses").innerHTML = "Guesses left: " + guesses;
 document.getElementById("used").innerHTML = "Letters guessed: " + lettersGuessed;
+
 }
    
    
